2c56fc1c51ab86963b312cdd823f46c520bb73d8
emanuelefirmani <emanuelefirmani@kpmg.com>
2025-04-06 17:36:59 +0200

    Refactoring: use of fold for tokens

--- src/parser.ts
+++ src/parser.ts
@@ -2,12 +2,26 @@ import {List} from 'immutable';
 import {Token} from "./tokenizer";
 
 type Node = number;
+type FoldItem = { accumulator: List<Node>, rest: List<Token> };
 
 export function parse(tokens: List<Token>): List<Node> {
-    return tokens.map(toNode);
+    const item = parse2({ accumulator: List<Node>(), rest: tokens});
+    return item.accumulator;
 }
 
-function toNode(token: Token): Node {
+function parse2(item: FoldItem): FoldItem {
+    if(item.rest.size === 0)
+        return item;
+
+    var token = item.rest.first();
+
+    const newAccumulator = item.accumulator.push(toNode(token));
+    const newRest = item.rest.skip(1);
+
+    return {accumulator: newAccumulator, rest: newRest};
+}
+
+function toNode(token: Token | undefined): Node {
     if(typeof token === 'number') {
         return token;
     }
