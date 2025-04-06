a52439d23a9e73c8861ef34b25ccecbaf1679179
emanuelefirmani <emanuelefirmani@kpmg.com>
2025-04-06 16:29:35 +0200

    Parser can parse numbers

--- src/parser.spec.ts
+++ src/parser.spec.ts
@@ -1,9 +1,17 @@
 import {parse} from "./parser";
 import {List} from "immutable";
+import {tokenize} from "./tokenizer";
 
 describe('parser', () => {
-    it('returns true', () => {
-       var result = parse(List());
-       expect(result).toEqual(true);
+    it('returns empty list', () => {
+        var tokens = tokenize("");
+        var result = parse(tokens);
+        expect(result.size).toEqual(0);
+    });
+
+    it('returns a number', () => {
+        var tokens = tokenize("42");
+        var result = parse(tokens);
+        expect(result.last()).toEqual(42);
     });
 })
--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,16 @@
 import {List} from 'immutable';
 import {Token} from "./tokenizer";
 
-export function parse(tokens: List<Token>): boolean {
-    return true;
+type Node = number;
+
+export function parse(tokens: List<Token>): List<Node> {
+    return tokens.map(toNode);
+}
+
+function toNode(token: Token): Node {
+    if(typeof token === 'number') {
+        return token;
+    }
+
+    throw new RangeError(`Unexpected token ${JSON.stringify(token)}`);
 }
